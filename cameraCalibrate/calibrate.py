46485009357b8c025668c1bb416af083a5e36838
Keegan Kelly <keegkelly2013@gmail.com>
2024-09-23 12:12:26 -0400

    added calibration

---
+++ cameraCalibrate/calibrate.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+import glob
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((7*4, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:4].T.reshape(-1, 2)
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+images = glob.glob("*.png")
+goodImages = 0
+for fname in images:
+    img = cv2.imread(fname)
+    if img is None:
+        print("Check file path")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7, 4), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        goodImages += 1
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (7, 4), corners2, ret)
+        #cv2.imshow('img', img)
+        # cv2.waitKey(500)
+cv2.destroyAllWindows()
+print(goodImages)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+print("Camera matrix: \n", mtx)
+np.savez('wideAngleCalibration.npz', mtx=mtx, dist=dist)
---
+++ cameraCalibrate/calibrateCapture.py
@@ -0,0 +1,25 @@
+"""
+Code Authored by Keegan Kelly
+"""
+import cv2
+import time
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+# lower focus focuses further away from the camera
+focus = 0  # min: 0, max: 255, increment:5
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap.set(cv2.CAP_PROP_FOCUS, focus)
+cap.set(cv2.CAP_PROP_FPS, 30)
+i = 0
+while 1:
+    ret, frame = cap.read()
+    if ret:
+
+        cv2.imshow("frame", frame)
+        cv2.imwrite("calibrationPhotos/chessPic"+str(i)+".png", frame)
+        time.sleep(0.5)
+        i += 1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
