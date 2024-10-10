78e06d99615e2462ba1f5d0de1bf0068eafdd42e
John Timpson <timpson.jo@gmail.com>
2024-10-10 12:19:19 -0400

    made smoth

--- src/handPositionFilter.py
+++ src/handPositionFilter.py
@@ -1,5 +1,6 @@
 #Class to control the moving average
 import numpy as np
+from scipy.filter import savgol_filter
 
 class rollingAvg:
     def __init__(self, n_avg):
@@ -9,15 +10,29 @@ class rollingAvg:
         self.stored = np.zeros((n_avg,3))
         self.index = 0
         
-    def addPos(self,pos):
+    def smoothPos(self,pos):
         self.stored[self.index,:] = pos
-        self.average = np.sum(self.stored, axis=0)
+        self.average = np.sum(self.stored, axis=0)/self.n_avg
         if self.index < self.n_avg - 1:
             self.index = self.index + 1
         else:
             self.index = 0
-                
-
+            
+        return self.average
+            
+            
+class SavitzkyGolay:
+    def __init__(self, window):
+        self.filtered = 0
+        self.windowLength = window
+        self.stored = np.zeros((n_avg,3))
+        self.index = 0
         
-        
+    def smoothPos(self,pos):
+        self.stored[self.index,:] = pos
+        self.filtered = savgol_filter(self.stored, self.windowLength, 2)
+        if self.index < self.n_avg - 1:
+            self.index = self.index + 1
+        else:
+            self.index = 0
     
