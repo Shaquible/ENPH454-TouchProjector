099416028f5e0bb11e4030815958fb8e05b89c95
John Timpson <timpson.jo@gmail.com>
2024-10-10 11:09:05 -0400

    tested and fixed the handPositionFilter class

--- src/handPositionFilter.py
+++ src/handPositionFilter.py
@@ -1,20 +1,23 @@
 #Class to control the moving average
+import numpy as np
 
-class handPostionFilter:
-    def __init__(self, n_avg)
+class rollingAvg:
+    def __init__(self, n_avg):
         # Defines the rolling average of the hand tracked position.
         self.average = 0
         self.n_avg = n_avg
-        self.stored = np.array((n_avg,3))
+        self.stored = np.zeros((n_avg,3))
         self.index = 0
         
-    def addPos(self,pos)
+    def addPos(self,pos):
         self.stored[self.index,:] = pos
-        self.average = sum(self.stored, axis=0)
-        if self.index < self.n_avg:
+        self.average = np.sum(self.stored, axis=0)
+        if self.index < self.n_avg - 1:
             self.index = self.index + 1
-            else
-                self.index = 0
+        else:
+            self.index = 0
+                
+
         
         
     
