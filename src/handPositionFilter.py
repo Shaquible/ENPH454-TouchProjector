e4afd260e8aef8384e1bb97f6d28094aefbe4275
John Timpson <timpson.jo@gmail.com>
2024-10-28 11:33:53 -0400

    Added PID control algorithm to handPositionFilter

--- src/handPositionFilter.py
+++ src/handPositionFilter.py
@@ -1,6 +1,6 @@
 #Class to control the moving average
 import numpy as np
-#from scipy.filter import savgol_filter
+from scipy.signal import savgol_filter
 
 class rollingAvg:
     def __init__(self, x_len, y_len, z_len):
@@ -15,30 +15,91 @@ class rollingAvg:
         self.xind = 0
         self.yind = 0
         self.zind = 0
+        self.pos = [0,0,0]
         
     def smoothPos(self,pos):
         self.xs[self.xind] = pos[0]
         self.ys[self.yind] = pos[1]
         self.zs[self.zind] = pos[2]
-        outPos = [np.mean(self.xs), np.mean(self.ys), np.mean(self.zs)]
+        self.pos = [np.mean(self.xs), np.mean(self.ys), np.mean(self.zs)]
         self.xind = (self.xind + 1)%self.x_len
         self.yind = (self.yind + 1)%self.y_len
         self.zind = (self.zind + 1)%self.z_len    
+        return self.pos
+        
+class rollingAvg_PID:
+    def __init__(self, tunMtx, x_len, y_len, z_len):
+        # Defines the rolling average of the hand tracked position.
+        self.average = 0
+        self.x_len = x_len
+        self.xs = np.zeros(x_len)
+        self.xerrs = np.zeros(x_len)
+        self.y_len = y_len
+        self.ys = np.zeros(y_len)
+        self.yerrs = np.zeros(y_len)
+        self.z_len = z_len
+        self.zs = np.zeros(z_len)
+        self.zerrs = np.zeros(z_len)
+        self.xind = 0
+        self.yind = 0
+        self.zind = 0
+        self.tunMtx = tunMtx
+        self.pos = [0,0,0]
+        
+    def der(vals, ind):
+        return 0.5*(vals[ind] - vals[ind-2])
+        
+    # This smooth position function includes a simple PID control algorithm as well to further smooth the system and rapid changes to the system.
+    def smoothPos(self,pos,imgPos, timeStep):
+        self.xs[self.xind] = pos[0]
+        self.ys[self.yind] = pos[1]
+        self.zs[self.zind] = pos[2]
+        filteredPos = [np.mean(self.xs), np.mean(self.ys), np.mean(self.zs)]
+        
+        self.xerrs[self.xind] =  filteredPos[0] - imgPos[0]
+        self.yerrs[self.yind] =  filteredPos[1] - imgPos[1]
+        self.zerrs[self.zind] =  filteredPos[2] - imgPos[2]
+        
+        xI = self.xI + self.xerrs[self.xind]*timeStep
+        yI = self.yI + self.yerrs[self.yind]*timeStep
+        zI = self.zI + self.zerrs[self.zind]*timeStep
+        
+        xD = (self.xerrs[self.xind] - self.xerrs[(self.xind-1)%self.x_len])/timeStep
+        yD = (self.yerrs[self.yind] - self.yerrs[(self.yind-1)%self.y_len])/timeStep
+        zD = (self.zerrs[self.zind] - self.zerrs[(self.zind-1)%self.z_len])/timeStep
+        
+        outPos = [0,0,0]
+        outPos[0] = self.tunMtx[0]*self.xerrs[self.xind] + self.tunMtx[1]*xI + self.tunMTX[2]*xD
+        outPos[1] = self.tunMtx[0]*self.yerrs[self.yind] + self.tunMtx[1]*yI + self.tunMTX[2]*yD
+        outPos[2] = self.tunMtx[0]*self.zerrs[self.zind] + self.tunMtx[1]*zI + self.tunMTX[2]*zD
+        
+        self.xind = (self.xind + 1)%self.x_len
+        self.yind = (self.yind + 1)%self.y_len
+        self.zind = (self.zind + 1)%self.z_len
+        
         return outPos
             
-            
 # class SavitzkyGolay:
-#     def __init__(self, window):
-#         self.filtered = 0
-#         self.windowLength = window
-#         self.stored = np.zeros((self.n_avg,3))
-#         self.index = 0
+#     def __init__(self, x_len, y_len, z_len):
+#         self.x_len = x_len
+#         self.xs = np.zeros(x_len)
+#         self.y_len = y_len
+#         self.ys = np.zeros(y_len)
+#         self.z_len = z_len
+#         self.zs = np.zeros(z_len)
+#         self.xind = 0
+#         self.yind = 0
+#         self.zind = 0
         
 #     def smoothPos(self,pos):
-#         self.stored[self.index,:] = pos
-#         self.filtered = savgol_filter(self.stored, self.windowLength, 2)
-#         if self.index < self.n_avg - 1:
-#             self.index = self.index + 1
-#         else:
-#             self.index = 0
+#         self.xs[self.xind] = pos[0]
+#         self.ys[self.yind] = pos[1]
+#         self.zs[self.zind] = pos[2]
+        
+#         self.xind = (self.xind + 1)%self.x_len
+#         self.yind = (self.yind + 1)%self.y_len
+#         self.zind = (self.zind + 1)%self.z_len    
+#         return outPos
+    
+
     
--- src/smoothingTest.py
+++ src/smoothingTest.py
@@ -1,13 +1,20 @@
 #This code tests the handPositionFilter code to see if it is actually smoothing the data well.
 import numpy as np
-from handPositionFilter import rollingAvg
+import matplotlib.pyplot as plt
+from handPositionFilter import rollingAvg_PID
 
 N = 10000
-handPosition = rollingAvg(30)
+handPosition = rollingAvg_PID([0.1, 0.1, 0.001],2,2,3)
 
-testData = np.random.rand(N,3)
-for i in range(N):
-    inputData = testData[i,:]
-    handPosition.addPos(inputData)
-    print(handPosition.average)
+testData = np.zeros(N)
+x = np.linspace(0,N-1,num = N)
+testData = (1/N**2)*(x**2)
+noise = 0.1*np.random.rand(N,1)
+testOutput = testData + noise
+for i in range(3,N):
+    inputData = testOutput[i]
+    imgPos = testData[i-2]
+    filteredData = handPosition.smoothPos(inputData, imgPos, 0.01)
     
+plt.plot(x,testData, 'rx')
+plt.plot(x,filteredData, 'bo')
