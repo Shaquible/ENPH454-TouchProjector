3bb0258ffef42d0972cf0edb78d3c8469db437ca
John Doe <keegkelly2013@gmail.com>
2024-10-31 19:25:33 -0400

    Updated filtering to use de emphasis

--- src/handPositionFilter.py
+++ src/handPositionFilter.py
@@ -1,7 +1,8 @@
-#Class to control the moving average
+# Class to control the moving average
 import numpy as np
 import time
-#from scipy.filter import savgol_filter
+# from scipy.filter import savgol_filter
+
 
 class rollingAvg:
     def __init__(self, x_len, y_len, z_len):
@@ -16,35 +17,35 @@ class rollingAvg:
         self.xind = 0
         self.yind = 0
         self.zind = 0
-        
-    def smoothPos(self,pos):
+
+    def smoothPos(self, pos):
         self.xs[self.xind] = pos[0]
         self.ys[self.yind] = pos[1]
         self.zs[self.zind] = pos[2]
         outPos = [np.mean(self.xs), np.mean(self.ys), np.mean(self.zs)]
-        self.xind = (self.xind + 1)%self.x_len
-        self.yind = (self.yind + 1)%self.y_len
-        self.zind = (self.zind + 1)%self.z_len    
+        self.xind = (self.xind + 1) % self.x_len
+        self.yind = (self.yind + 1) % self.y_len
+        self.zind = (self.zind + 1) % self.z_len
         return outPos
-            
+
+
 class lowPassVelocity:
     def __init__(self, xWindow, yWindow, zWindow):
         self.prev = np.zeros(3)
         self.prevOut = np.zeros(3)
         self.prevTime = time.time()
-        self.vXs  = np.zeros(xWindow)
-        self.vYs  = np.zeros(yWindow)
-        self.vZs  = np.zeros(zWindow)
+        self.vXs = np.zeros(xWindow)
+        self.vYs = np.zeros(yWindow)
+        self.vZs = np.zeros(zWindow)
         self.xind = 0
         self.yind = 0
         self.zind = 0
         self.xWindow = xWindow
         self.yWindow = yWindow
         self.zWindow = zWindow
-        
-        
-    def smoothPos(self,pos):
-        #update velocity based on the change in position measured
+
+    def smoothPos(self, pos):
+        # update velocity based on the change in position measured
         t = time.time()
         dt = t - self.prevTime
         self.prevTime = t
@@ -53,13 +54,26 @@ class lowPassVelocity:
         self.vXs[self.xind] = vel[0]
         self.vYs[self.yind] = vel[1]
         self.vZs[self.zind] = vel[2]
-        outVel = np.array([np.mean(self.vXs), np.mean(self.vYs), np.mean(self.vZs)])
-        self.xind = (self.xind + 1)%self.xWindow
-        self.yind = (self.yind + 1)%self.yWindow
-        self.zind = (self.zind + 1)%self.zWindow
-        #update position based on the velocity and the previous position
+        outVel = np.array(
+            [np.mean(self.vXs), np.mean(self.vYs), np.mean(self.vZs)])
+        self.xind = (self.xind + 1) % self.xWindow
+        self.yind = (self.yind + 1) % self.yWindow
+        self.zind = (self.zind + 1) % self.zWindow
+        # update position based on the velocity and the previous position
         self.prevOut = self.prevOut + outVel*dt
         return self.prevOut
+
+
+class deEmphasis:
+    def __init__(self, coef=0.95):
+        self.coef = coef
+        self.prev = np.zeros(4)
+
+    def smoothPos(self, pos):
+        out = (pos + self.prev*(self.coef))/(1+self.coef)
+        self.prev = out
+        return out
+
 
 # class SavitzkyGolay:
 #     def __init__(self, window):
@@ -67,7 +81,7 @@ class lowPassVelocity:
 #         self.windowLength = window
 #         self.stored = np.zeros((self.n_avg,3))
 #         self.index = 0
-        
+
 #     def smoothPos(self,pos):
 #         self.stored[self.index,:] = pos
 #         self.filtered = savgol_filter(self.stored, self.windowLength, 2)
@@ -75,4 +89,3 @@ class lowPassVelocity:
 #             self.index = self.index + 1
 #         else:
 #             self.index = 0
-    
--- src/main.py
+++ src/main.py
@@ -6,21 +6,23 @@ import numpy as np
 from multiprocessing import Process, Queue
 import time
 from mouseMove import mouseMove
-from handPositionFilter import lowPassVelocity
+from handPositionFilter import deEmphasis
 import cv2
 import pandas as pd
 imHeight = 1080
 imWidth = 1920
 exposure = -8
+
+
 def ML_Process(captureNum: int, cropRegion, dataQueue: Queue, capSQ: Queue, capRQ: Queue, processSQ: Queue, processRQ: Queue):
     opened = False
     while not opened:
         cap = openStream(captureNum, 1080, 1920, exposure=-8)
         if cap.isOpened():
             opened = True
-    
+
     Tracker = HandTracker(cap, captureNum, cropRegion)
-    #Tracker.drawDebug = False
+    # Tracker.drawDebug = False
     Tracker.startCapture(capSQ, capRQ)
     time.sleep(0.2)
     Tracker.startHandTracking(dataQueue, processSQ, processRQ)
@@ -28,7 +30,7 @@ def ML_Process(captureNum: int, cropRegion, dataQueue: Queue, capSQ: Queue, capR
 
 
 def main():
-   
+
     mp_hands = mp.solutions.hands
     markerWidth = 0.1586
     npfile = np.load("cameraIntrinsics/IRCam1.npz")
@@ -45,9 +47,9 @@ def main():
         ret2, frame2 = cap2.read()
         if ret1 and ret2:
             break
-    crop1 = cv2.selectROI("cam0",frame1)
+    crop1 = cv2.selectROI("cam0", frame1)
     cv2.destroyAllWindows()
-    crop2 = cv2.selectROI("cam1",frame2)
+    crop2 = cv2.selectROI("cam1", frame2)
     cv2.destroyAllWindows()
     print(crop1, crop2)
 
@@ -57,7 +59,7 @@ def main():
     cap1.release()
     cap2.release()
     time.sleep(0.2)
-    positionFilter = lowPassVelocity(2, 5, 3)
+    positionFilter = deEmphasis()
     # launching the process to run tracking on each camera
     procs = []
     q1 = Queue(1)
@@ -66,8 +68,10 @@ def main():
     capQ2 = Queue(1)
     processQ1 = Queue(1)
     processQ2 = Queue(1)
-    procs.append(Process(target=ML_Process, args=(0, crop1, q1, capQ1, capQ2, processQ1, processQ2)))
-    procs.append(Process(target=ML_Process, args=(1, crop2, q2, capQ2, capQ1, processQ2, processQ1)))
+    procs.append(Process(target=ML_Process, args=(
+        0, crop1, q1, capQ1, capQ2, processQ1, processQ2)))
+    procs.append(Process(target=ML_Process, args=(
+        1, crop2, q2, capQ2, capQ1, processQ2, processQ1)))
     for proc in procs:
         proc.start()
     mouse = mouseMove()
@@ -85,10 +89,10 @@ def main():
             if cam1Hands is not None and cam2Hands is not None:
                 for hand in cam1Hands:
                     cam1Coords = np.array([(hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x)*crop1[2] + crop1[0],
-                                           (hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y)*crop1[3]  + crop1[1]])
+                                           (hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y)*crop1[3] + crop1[1]])
                 for hand in cam2Hands:
                     cam2Coords = np.array([(hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x)*crop2[2] + crop2[0],
-                                           (hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y)*crop2[3]+ crop2[1]])
+                                           (hand.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y)*crop2[3] + crop2[1]])
                 pos = tri.get3dPoint(cam1Coords, cam2Coords)[:, 0]
                 # if i < dataCollectLen:
                 #     times[i] = time.time()
@@ -101,17 +105,18 @@ def main():
                 #     df.to_csv("dataCapture.csv")
                 #     i += 1
                 #     print("END\n")
-                
+
                 dt = time.time() - t0
                 t0 = time.time()
 
-                #pos = positionFilter.smoothPos(pos[:3])
-                position = "X: {:.2f} Y: {:.2f} Z: {:.2f} dt{:.3f}".format(pos[0]*100, pos[1]*100, pos[2]*100, dt)
+                pos = positionFilter.smoothPos(pos[:3])
+                position = "X: {:.2f} Y: {:.2f} Z: {:.2f} dt{:.3f}".format(
+                    pos[0]*100, pos[1]*100, pos[2]*100, dt)
                 print(position, end="\r")
                 mouse.moveMouse(pos)
 
-                #print(pos, end="\r")
-                
+                # print(pos, end="\r")
+
     except KeyboardInterrupt:
         print("\nShutting down")
         for proc in procs:
