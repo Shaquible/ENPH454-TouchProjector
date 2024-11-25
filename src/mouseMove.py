b974b5e20953c4467ffe9c67dee5fbcc2131b4f3
John Timpson <timpson.jo@gmail.com>
2024-11-25 13:59:14 -0500

    Added gesture to mousemove

--- src/mouseMove.py
+++ src/mouseMove.py
@@ -11,11 +11,14 @@ class mouseMove:
         self.lastState = False
         pyautogui.FAILSAFE = False
         self.debounceZ = Debouncer(2, False)
+        self.gestDebounce = Debouncer(5, False)
+        self.prev_sign_id = 0
+
         for m in get_monitors():
             self.xRes = m.width
             self.yRes = m.height
 
-    def moveMouse(self, position, leftClick=True):
+    def moveMouse(self, position, hand_sign_id, gestureToggle=True, leftClick=True):
         xy = np.array([position[0], position[1], 1])
         uv = np.matmul(self.tranform, xy)
         x = uv[0]/uv[2]
@@ -39,6 +42,17 @@ class mouseMove:
             self.lastState = False
         else:
             pyautogui.moveTo(x, y, duration=0.0001 , _pause=False)
+            
+        #Gesture Shortcut included into mousemove
+        if gestureToggle == 1:
+            hand_sign_id_db = self.gestDebounce.debounce(hand_sign_id)
+            if hand_sign_id_db != self.prev_sign_id:
+                if hand_sign_id_db == 0:
+                    pyautogui.press("E")
+                elif hand_sign_id_db == 2:
+                    pyautogui.press("B")
+            self.prev_sign_id = hand_sign_id_db
+            
         return
     
     # def unclick(self):
