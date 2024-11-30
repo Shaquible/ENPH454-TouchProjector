1bdd6cb1aa3ed1194d0c3e1689bd3a5b74de2d5a
John Doe <keegkelly2013@gmail.com>
2024-11-29 19:19:03 -0500

    pushed final data

--- plotResVTimes.py
+++ plotResVTimes.py
@@ -1,7 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-data = np.loadtxt("handTrackerResults.csv", delimiter=",")
-plt.plot(data[:, 0], data[:, 1], "ro")
+data1 = np.loadtxt("handTrackerResults.csv", delimiter=",")
+data2 = np.loadtxt("handTrackerResultsWithHSVConversion.csv", delimiter=",")
+# find the slope of the line
+m1 = np.mean(np.diff(data1[:, 1])/np.diff(data1[:, 0]))
+m2 = np.mean(np.diff(data2[:, 1])/np.diff(data2[:, 0]))
+print(m1, m2)
+plt.plot(data1[:, 0], data1[:, 1]*1000, "ro", label="Inference")
+plt.plot(data2[:, 0], data2[:, 1]*1000, "bo",
+         label="Inference + Image Recoloring")
+plt.text(0, 0.5, "Inference Slope: " + str(m1), fontsize=12)
+plt.text(0, 0.4, "Inference + Image Recoloring Slope: " + str(m2), fontsize=12)
 plt.xlabel("Number of Pixels")
-plt.ylabel("Time (s)")
-plt.show()
+plt.ylabel("Frame Time (ms)")
+plt.legend()
+plt.savefig("handTrackerResults.png")
